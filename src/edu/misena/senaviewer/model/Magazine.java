797b345ce672ed713a12c1b2061bd4f68df5cd20
SAntiGB05 <santigbttobi@gmail.com>
2024-08-15 09:25:11 -0500

    se creo el constructor de la cass Magazine

--- src/edu/misena/senaviewer/model/Magazine.java
+++ src/edu/misena/senaviewer/model/Magazine.java
@@ -9,4 +9,10 @@ public class Magazine {
     Date edititionDate;
     String editorial;
     String[] authors = new String[5];
+
+    public Magazine(String title,Date edititionDate,String editorial){
+        this.title = title;
+        this.edititionDate = edititionDate;
+        this.editorial = editorial;
+    }
 }
