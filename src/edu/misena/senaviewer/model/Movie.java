1b87a49b8785d85fd033e60cbcc1ce4b1c6c3aa9
SAntiGB05 <santigbttobi@gmail.com>
2024-08-15 08:51:39 -0500

    se creo la class Movie con sus metodos getters y setters

--- src/edu/misena/senaviewer/model/Movie.java
+++ src/edu/misena/senaviewer/model/Movie.java
@@ -1,4 +1,27 @@
 package edu.misena.senaviewer.model;
 
 public class Movie {
+
+    int id;
+    String title;
+
+    String gender;
+    String creator;
+    int duration;
+    short year;
+
+    boolean viewed;
+    int timeViewed;
+
+    public static void see(){
+
+    }
+
+    public static void getters(){
+
+    }
+
+    public static void setters(){
+
+    }
 }
