92bd2e73a04f6da8aab00171927db553c1ad1b31
SAntiGB05 <santigbttobi@gmail.com>
2024-08-15 10:36:03 -0500

    se realizaron los metodos setter y getter en la clase Book

--- src/edu/misena/senaviewer/model/Book.java
+++ src/edu/misena/senaviewer/model/Book.java
@@ -7,7 +7,7 @@ public class Book {
     int id;
     String title;
 
-    Date edititionDate;
+    Date editionDate;
     String editorial;
     String[] authors;
 
@@ -15,19 +15,79 @@ public class Book {
     boolean readed;
     int timeReaded;
 
-    public Book (String title,Date edititionDate, String editorial, int isbn){
+    public Book (String title,Date editionDate, String editorial, int isbn){
         this.title = title;
-        this.edititionDate = edititionDate;
+        this.editionDate = editionDate;
         this.editorial = editorial;
         this.isbn = isbn;
     }
 
-    public static void getters(){
-
+    public int getId(){
+        return id;
     }
 
-    public static void setters(){
+    public void setId(int id){
+        this.id = id;
+    }
 
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public Date getEditionDate(){
+        return editionDate;
+    }
+
+    public void setEditionDate(Date editionDate){
+        this.editionDate = editionDate;
+    }
+
+    public String getEditorial(){
+        return editorial;
+    }
+
+    public void setEditorial(String editorial){
+        this.editorial = editorial;
+    }
+
+    public String[] getAuthors(){
+        return authors;
+    }
+
+    public void setAuthors(String[] authors) {
+        if (authors == null) {
+            this.authors = new String[0];
+        } else {
+            this.authors = authors;
+        }
+    }
+
+    public int getIsbn(){
+        return isbn;
+    }
+
+    public void setIsbn(int isbn){
+        this.isbn = isbn;
+    }
+
+    public boolean getReaded(){
+        return readed;
+    }
+
+    public void setReaded(boolean readed){
+        this.readed = readed;
+    }
+
+    public int getTimeReaded(){
+        return timeReaded;
+    }
+
+    public void setTimeReaded(int timeReaded){
+        this.timeReaded = timeReaded;
     }
 
 }
