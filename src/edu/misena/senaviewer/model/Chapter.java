45345c0508f269d85ec48ae31e980b2218d7744e
SAntiGB05 <santigbttobi@gmail.com>
2024-08-15 08:36:22 -0500

    se creo la class Chapter y se le realizo el constructor

--- src/edu/misena/senaviewer/model/Chapter.java
+++ src/edu/misena/senaviewer/model/Chapter.java
@@ -1,4 +1,26 @@
 package edu.misena.senaviewer.model;
 
 public class Chapter {
+    int id;
+    String title;
+
+    int duration;
+    short year;
+    boolean viewed;
+
+    int timeViewed;
+    int sessionNumber;
+
+    public static void see(){
+
+    }
+
+    public static void getters(){
+
+    }
+
+    public static void setters(){
+
+    }
+
 }
