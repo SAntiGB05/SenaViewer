8eba862a8ebb15d532b5b2bfeb178c5012e7e4a7
SAntiGB05 <santigbttobi@gmail.com>
2024-08-15 08:44:20 -0500

    se creo la class Book

--- src/edu/misena/senaviewer/model/Book.java
+++ src/edu/misena/senaviewer/model/Book.java
@@ -1,4 +1,30 @@
 package edu.misena.senaviewer.model;
 
+import java.util.Date;
+
 public class Book {
+
+    int id;
+    String title;
+
+    Date edititionDate;
+    String editorial;
+    String authors;
+
+    int isbn;
+    boolean readed;
+    int timeReaded;
+
+    public static void see(){
+
+    }
+
+    public static void getters(){
+
+    }
+
+    public static void setters(){
+
+    }
+
 }
