ef2e7c2f1945892d06a55f5107727383b667a179
SAntiGB05 <santigbttobi@gmail.com>
2024-08-21 14:41:06 -0500

    se crearon los set y get para la clase movie

--- src/edu/misena/senaviewer/model/Movie.java
+++ src/edu/misena/senaviewer/model/Movie.java
@@ -21,11 +21,67 @@ public class Movie {
         this.year = year;
     }
 
-    public static void getters(){
-
+    public int getId() {
+        return id;
     }
 
-    public static void setters(){
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public short getYear() {
+        return year;
+    }
+
+    public void setYear(short year) {
+        this.year = year;
+    }
+
+    public boolean getViewed() {
+        return viewed;
+    }
+
+    public void setViewed(boolean viewed) {
+        this.viewed = viewed;
+    }
+
+    public int getTimeViewed() {
+        return timeViewed;
+    }
+
+    public void setTimeViewed(int timeViewed) {
+        this.timeViewed = timeViewed;
     }
 }
