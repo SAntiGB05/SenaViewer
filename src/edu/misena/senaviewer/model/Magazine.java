75807ac6af33292266ab6938e547ca7711952a1b
SAntiGB05 <santigbttobi@gmail.com>
2024-08-21 14:37:09 -0500

    se crearon los set y get para la clase magazine

--- src/edu/misena/senaviewer/model/Magazine.java
+++ src/edu/misena/senaviewer/model/Magazine.java
@@ -6,13 +6,53 @@ public class Magazine {
 
     int id;
     String title;
-    Date edititionDate;
+    Date editionDate;
     String editorial;
     String[] authors;
 
     public Magazine(String title,Date edititionDate,String editorial){
         this.title = title;
-        this.edititionDate = edititionDate;
+        this.editionDate = edititionDate;
         this.editorial = editorial;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getEditionDate() {
+        return editionDate;
+    }
+
+    public void setEditionDate(Date editionDate) {
+        this.editionDate = editionDate;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public String[] getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String[] authors) {
+        this.authors = authors;
     }
 }
