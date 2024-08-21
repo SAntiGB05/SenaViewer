f1366ed86331a5e65e670dcca6855d0f6b9093a7
SAntiGB05 <santigbttobi@gmail.com>
2024-08-21 14:28:49 -0500

    se crearon los set y get para la clase chapter

--- src/edu/misena/senaviewer/model/Chapter.java
+++ src/edu/misena/senaviewer/model/Chapter.java
@@ -17,12 +17,59 @@ public class Chapter {
         this.year = year;
     }
 
-    public static void getters(){
-
+    public int getId(){
+        return id;
     }
 
-    public static void setters(){
-
+    public void setId(int id){
+        this.id = id;
     }
 
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public int getDuration(){
+        return duration;
+    }
+
+    public void setDuration(int duration){
+        this.duration = duration;
+    }
+
+    public short getYear(){
+        return year;
+    }
+
+    public void setYear(short year){
+        this.year = year;
+    }
+
+    public boolean getViewed (){
+        return viewed;
+    }
+
+    public void setViewed(boolean viewed){
+        this.viewed = viewed;
+    }
+
+    public int getTimeViewed() {
+        return timeViewed;
+    }
+
+    public void setTimeViewed(int timeViewed) {
+        this.timeViewed = timeViewed;
+    }
+
+    public int getSessionNumber() {
+        return sessionNumber;
+    }
+
+    public void setSessionNumber(int sessionNumber) {
+        this.sessionNumber = sessionNumber;
+    }
 }
