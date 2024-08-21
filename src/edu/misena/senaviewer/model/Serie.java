6cf956c4b127c75cd04d10010d3e29ad1ada8338
SAntiGB05 <santigbttobi@gmail.com>
2024-08-21 14:42:54 -0500

    se crearon los set y get para la clase serie

--- src/edu/misena/senaviewer/model/Serie.java
+++ src/edu/misena/senaviewer/model/Serie.java
@@ -20,12 +20,75 @@ public class Serie {
         this.duration = duration;
     }
 
-    public static void getters(){
-
+    public int getId() {
+        return id;
     }
 
-    public static void setters(){
-
+    public void setId(int id) {
+        this.id = id;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public short getYear() {
+        return year;
+    }
+
+    public void setYear(short year) {
+        this.year = year;
+    }
+
+    public boolean getViewed() {
+        return viewed;
+    }
+
+    public void setViewed(boolean viewed) {
+        this.viewed = viewed;
+    }
+
+    public int getTimeViewed() {
+        return timeViewed;
+    }
+
+    public void setTimeViewed(int timeViewed) {
+        this.timeViewed = timeViewed;
+    }
+
+    public int getSessionQuantity() {
+        return sessionQuantity;
+    }
+
+    public void setSessionQuantity(int sessionQuantity) {
+        this.sessionQuantity = sessionQuantity;
+    }
 }
